c3cf0d84fce4debefd04bbfd24c9af53e7318de3
Mohammed <atefhamosa@gmail.com>
2021-02-03 19:25:27 +0200

    Rubocop correct

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-    
+
   def index
-      @users = User.all
+    @users = User.all
   end
 
   def new
@@ -27,15 +27,15 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-        if @user.update(users_params)
-        redirect_to @user
+    if @user.update(users_params)
+      redirect_to @user
     else
       render 'edit'
     end
   end
 
   private
-  
+
   def users_params
     params.require(:user).permit(:username, :email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}
-    validates :email, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}
-    validates :password, presence: true, length: {maximum: 50}
+  validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
+  validates :password, presence: true, length: { maximum: 50 }
 end
