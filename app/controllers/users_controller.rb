13c16924523a637c9b7aae873ab7fb4b1e52d830
icebox827 <icebox827@gmail.com>
2021-02-03 10:41:51 -0500

    update form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
-    def new
-        
-    end
+  def new; end
 
-    def create
-        
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
-    
-    
+  end
+
+  private
+  
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
